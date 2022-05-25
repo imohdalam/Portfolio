bf332ea6bf5999c516b17989c8868fe090860a96
imohdalam <mdshabbiralam1@gmail.com>
2022-05-25 14:25:28 +0530

    Update in section

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('controls');
+const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 // Function for page transition by using icons.
 function PageTransitions(){
@@ -13,9 +13,27 @@ function PageTransitions(){
             this.className += ' active-btn'; 
         })
     }
+
+    // Section active
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            // Remove selected from the other Btns
+            sectBtns.forEach((sectBtn)=>{
+                sectBtn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // Hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
-
-// Active section 
 
